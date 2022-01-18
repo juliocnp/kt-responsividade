ccc20e88d1efbd9ff7cdd0431a2db46bf644fa8b
Julio Nunes <julio.nunes@dtidigital.com.br>
2022-01-18 10:48:13 -0300

    exemplo

--- src/App.js
+++ src/App.js
@@ -3,21 +3,139 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <div className="header">
+        <img src={logo} className="header--logo" alt="logo" />
+        <span className="header--titulo">Exemplinho</span>
+        <div className="header--opcoes">
+          <div className="header--opcao">
+            ITEM 1
+          </div>
+          <div className="header--opcao">
+            ITEM 2
+          </div>
+        </div>
+      </div>
+
+      <div className="menu">
+          <div className="menu--opcao">
+            ITEM 1
+          </div>
+          <div className="menu--opcao">
+            ITEM 2
+          </div>
+          <div className="menu--opcao">
+            ITEM 3
+          </div>
+          <div className="menu--opcao">
+            ITEM 4
+          </div>
+          <div className="menu--opcao">
+            ITEM 5
+          </div>
+      </div>
+
+      <div className="items">
+        <div className="items--card">
+          <div className="card--titulo"><span>ITEM 1</span></div>
+          <div className="card--foto"><span>FOTO</span></div>
+          <div className="card--botoes">
+            <div className="botao"><span>BOTAO 1</span></div>
+            <div className="botao"><span>BOTAO 2</span></div>
+          </div>
+        </div>
+        <div className="items--card">
+          <div className="card--titulo"><span>ITEM 1</span></div>
+          <div className="card--foto"><span>FOTO</span></div>
+          <div className="card--botoes">
+            <div className="botao"><span>BOTAO 1</span></div>
+            <div className="botao"><span>BOTAO 2</span></div>
+          </div>
+        </div>
+        <div className="items--card">
+          <div className="card--titulo"><span>ITEM 1</span></div>
+          <div className="card--foto"><span>FOTO</span></div>
+          <div className="card--botoes">
+            <div className="botao"><span>BOTAO 1</span></div>
+            <div className="botao"><span>BOTAO 2</span></div>
+          </div>
+        </div>
+        <div className="items--card">
+          <div className="card--titulo"><span>ITEM 1</span></div>
+          <div className="card--foto"><span>FOTO</span></div>
+          <div className="card--botoes">
+            <div className="botao"><span>BOTAO 1</span></div>
+            <div className="botao"><span>BOTAO 2</span></div>
+          </div>
+        </div>
+        <div className="items--card">
+          <div className="card--titulo"><span>ITEM 2</span></div>
+          <div className="card--foto"><span>FOTO</span></div>
+          <div className="card--botoes">
+            <div className="botao"><span>BOTAO 1</span></div>
+            <div className="botao"><span>BOTAO 2</span></div>
+          </div>
+        </div>
+        <div className="items--card">
+          <div className="card--titulo"><span>ITEM 3</span></div>
+          <div className="card--foto"><span>FOTO</span></div>
+          <div className="card--botoes">
+            <div className="botao"><span>BOTAO 1</span></div>
+            <div className="botao"><span>BOTAO 2</span></div>
+          </div>
+        </div>
+        <div className="items--card">
+          <div className="card--titulo"><span>ITEM 4</span></div>
+          <div className="card--foto"><span>FOTO</span></div>
+          <div className="card--botoes">
+            <div className="botao"><span>BOTAO 1</span></div>
+            <div className="botao"><span>BOTAO 2</span></div>
+          </div>
+        </div>
+        <div className="items--card">
+          <div className="card--titulo"><span>ITEM 5</span></div>
+          <div className="card--foto"><span>FOTO</span></div>
+          <div className="card--botoes">
+            <div className="botao"><span>BOTAO 1</span></div>
+            <div className="botao"><span>BOTAO 2</span></div>
+          </div>
+        </div>
+        <div className="items--card">
+          <div className="card--titulo"><span>ITEM 6</span></div>
+          <div className="card--foto"><span>FOTO</span></div>
+          <div className="card--botoes">
+            <div className="botao"><span>BOTAO 1</span></div>
+            <div className="botao"><span>BOTAO 2</span></div>
+          </div>
+        </div>
+        <div className="items--card">
+          <div className="card--titulo"><span>ITEM 7</span></div>
+          <div className="card--foto"><span>FOTO</span></div>
+          <div className="card--botoes">
+            <div className="botao"><span>BOTAO 1</span></div>
+            <div className="botao"><span>BOTAO 2</span></div>
+          </div>
+        </div>
+        <div className="items--card">
+          <div className="card--titulo"><span>ITEM 8</span></div>
+          <div className="card--foto"><span>FOTO</span></div>
+          <div className="card--botoes">
+            <div className="botao"><span>BOTAO 1</span></div>
+            <div className="botao"><span>BOTAO 2</span></div>
+          </div>
+        </div>
+        <div className="items--card">
+          <div className="card--titulo"><span>ITEM 9</span></div>
+          <div className="card--foto"><span>FOTO</span></div>
+          <div className="card--botoes">
+            <div className="botao"><span>BOTAO 1</span></div>
+            <div className="botao"><span>BOTAO 2</span></div>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
